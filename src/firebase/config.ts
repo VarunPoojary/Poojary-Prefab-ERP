f0f86140da6545744d6189a7b847e5a2f883b284
Varun Poojary <varunnpoojary@gmail.com>
2026-01-06 20:42:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8429595790-467d9",
+  "appId": "1:94475497504:web:f1edc757f76e4d70d9cc45",
+  "apiKey": "AIzaSyBZxMrNuxkrSDIHeQQas1qEHi7ZEdboSqw",
+  "authDomain": "studio-8429595790-467d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "94475497504"
+};
